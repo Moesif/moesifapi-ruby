a945299fbec23150c9d755b6b5b126fa605383cd
xinghengwang <xing@moesif.com>
2023-07-17 16:02:11 -0700

    more meaningful variable name

--- lib/moesif_api/controllers/base_controller.rb
+++ lib/moesif_api/controllers/base_controller.rb
@@ -2,10 +2,10 @@ module MoesifApi
   class BaseController
     attr_accessor :http_client, :http_call_back
 
-    @@http_client = FaradayClient.new
+    @@faraday_http_client = FaradayClient.new
 
     def initialize(http_client: nil, http_call_back: nil)
-      @http_client = http_client ||= @@http_client
+      @http_client = http_client ||= @@faraday_http_client
       @http_call_back = http_call_back
     end
 
