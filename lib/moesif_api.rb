87c35f69d0d7e6d59f8e90917b92fc74558a989b
xinghengwang <xing@moesif.com>
2023-07-17 15:57:08 -0700

    started with switching to faraday for making api calls

--- lib/moesif_api.rb
+++ lib/moesif_api.rb
@@ -19,6 +19,7 @@ require_relative 'moesif_api/http/http_request.rb'
 require_relative 'moesif_api/http/http_response.rb'
 require_relative 'moesif_api/http/http_context.rb'
 require_relative 'moesif_api/http/unirest_client.rb'
+require_relative 'moesif_api/http/faraday_client.rb'
 
 # Models
 require_relative 'moesif_api/models/base_model.rb'
---
+++ lib/moesif_api/http/faraday_client.rb
@@ -0,0 +1,50 @@
+require 'faraday'
+require 'faraday/net_http_persistent'
+
+module MoesifApi
+  class FaradayClient < HttpClient
+    def initialize
+      super()
+      @connection = Faraday.new({}) do |f|
+        f.adapter :net_http_persistent, pool_size: 5 do |http|
+          http.idle_timeout = 100
+        end
+      end
+    end
+
+    # Method overridden from HttpClient.
+    def execute_as_string(http_request)
+      if http_request.username || http_request.password
+        auth = { user: http_request.username, password: http_request.password }
+      end
+      response = @connection.run_request(http_request.http_method.downcase.to_sym,
+                                         http_request.query_url,
+                                         http_request.parameters, # body
+                                         http_request.headers)
+
+      # response = Unirest.method(http_request.http_method.downcase).call(http_request.query_url,
+      #                                                                   headers: http_request.headers, parameters: http_request.parameters,
+      #                                                                   auth: auth)
+
+      convert_response(response)
+    end
+
+    # Method overridden from HttpClient.
+    def execute_as_binary(http_request)
+      if http_request.username || http_request.password
+        auth = { user: http_request.username, password: http_request.password }
+      end
+
+      response = Unirest.method(http_request.http_method.downcase).call(http_request.query_url,
+                                                                        headers: http_request.headers, parameters: http_request.parameters,
+                                                                        auth: auth)
+
+      convert_response(response)
+    end
+
+    # Method overridden from HttpClient.
+    def convert_response(response)
+      HttpResponse.new(response.status, response.headers.dup, response.body)
+    end
+  end
+end
